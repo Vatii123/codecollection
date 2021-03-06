f88c34b49b4421d971ad5df3ab0b30eb9126a9bc
jar <66106915+Vatii123@users.noreply.github.com>
2021-03-06 13:01:20 +0700

    python
    a really simple python thing

---
+++ simple_python_code.py
@@ -0,0 +1,7 @@
+__author__ = 'Vatii123'
+
+print("Hello, I am Python. A coding language that is easy to understand more than Kotlin and other weird language.")
+print("ok. tell something about u")
+info = input()
+print("ok, i am selling " + info + " in the dark web thx")
+print("code author : vatii123")
