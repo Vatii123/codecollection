c6a269c55aaf25976a4cae80aaedad72aa52a59d
jar <66106915+Vatii123@users.noreply.github.com>
2021-03-06 12:34:34 +0700

    java
    not javascript but java

---
+++ println.java
@@ -0,0 +1,8 @@
+public class println {
+    public static void main(String[] args) {
+        System.out.println("simple java");
+        System.out.print("why does it need to be this complicated just to log console");
+        System.out.println("Reject Kotlin & JavaScript");
+        System.out.println("Return to Java");
+    }
+}
